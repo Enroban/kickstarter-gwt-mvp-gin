946461342d8c45df468baeefec3e950ef967c08b
Céline Louvet <celine.louvet@gmail.com>
2013-01-06 01:04:07 +0100

    Initial import

---
+++ src/main/java/org/kercoin/jeanne/client/JeanneEntryPoint.java
@@ -0,0 +1,10 @@
+package org.kercoin.jeanne.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class JeanneEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+}
